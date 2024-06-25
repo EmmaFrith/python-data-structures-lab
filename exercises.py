f315da84a85848adf0114fd18cb1608b762494ec
Emma Frith <emma.frith@education.gov.uk>
2024-06-25 23:40:15 +0100

    do exercise 4

--- exercises.py
+++ exercises.py
@@ -61,15 +61,68 @@ def combine_foods():
     foods = ('carrot', 'cake', 'pie')
     meal = ''
 
-    single_food = foods[0]
-    print(single_food)
-
-    concat_food_and_meal = single_food + meal
-    print(concat_food_and_meal)
-
-    # for food in foods:
-    #     meal += food 
-    # return meal    
+    for food in foods:
+        meal = meal + " " + food
+    return meal
 
 # Call the function and print the result
 print('Exercise 2:', combine_foods())
+
+
+
+
+
+
+
+# # Exercise 3: Slicing Tuples
+# #
+# # Using the slice operator, assign a new tuple containing only the last two food strings in the foods to a variable named last_two_foods.
+
+def slice_foods():
+    foods = ('carrot', 'cake', 'pie')
+    last_two_foods = foods[-2:]  # Slice the tuple to get the last two elements
+    return last_two_foods
+
+# Call the function and print the result
+print('Exercise 3:', slice_foods())
+
+
+
+
+
+
+# Exercise 4: Dictionaries and String Formatting
+#
+# Create a dictionary named home_town containing the keys of city, state, and population.
+
+# Using the home_town dictionary, assign to a variable named home_town_message a string with this format: “I was born in <city>, <state> - population of <population>”
+
+
+def hometown_info():
+
+    # your code here
+
+    home_town = {
+        'city': '',
+        'state': '',
+        'population': ''
+    }
+
+    home_town['city'] = 'Chicago'
+    # print(home_town)
+
+    home_town['state'] = 'Illinois'
+    # print(home_town)
+
+    home_town['population'] = 'nearly 3 mil'
+    # print(home_town)
+
+    home_town_message = f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}"
+    return home_town_message
+
+# Call the function and print the result
+print('Exercise 4:', hometown_info())
+
+
+
+
